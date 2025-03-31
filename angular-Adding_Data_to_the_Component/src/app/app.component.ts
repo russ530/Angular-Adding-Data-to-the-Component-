5f62ec86fb1b10df7b6750bedd67588341df2593
russ530 guevarra <guevarra.russel@iisgalvanimi.edu.it>
2025-03-31 11:58:30 +0000

    working with array

--- angular-Adding_Data_to_the_Component/src/app/app.component.ts
+++ angular-Adding_Data_to_the_Component/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserItemComponent } from './user-item/user-item.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserItemComponent, HelloWorldComponent],
+  imports: [RouterOutlet, UserItemComponent, HelloWorldComponent, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
